c93dac73b3e41cf5c54483998afc359bf3ee01b1
pugthepug88 <jinrcao@gmail.com>
2026-08-19 00:27:46 +1000

    Make email template carousel backing transparent

--- src/components/v5/scene-email-polished.tsx
+++ src/components/v5/scene-email-polished.tsx
@@ -1,1 +1,15 @@
-export { SceneEmailDirect as SceneEmailPolished } from "./scene-email-direct";
+import { SceneEmailDirect } from "./scene-email-direct";
+import { type SceneProps } from "./motion-kit";
+
+export function SceneEmailPolished(props: SceneProps) {
+  return (
+    <div className="email-carousel-transparent absolute inset-0 overflow-hidden">
+      <style>{`
+        .email-carousel-transparent div[class*="right-[1%]"][class*="w-[49%]"][class*="bg-[#f7f8fb]"] {
+          background: transparent !important;
+        }
+      `}</style>
+      <SceneEmailDirect {...props} />
+    </div>
+  );
+}
