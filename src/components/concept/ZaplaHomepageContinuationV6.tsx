65e407ad19f940c2886b96cabf335049b98e1462
pugthepug88 <jinrcao@gmail.com>
2026-09-01 23:38:55 +1000

    Fix V6 AI import so full page renders

--- src/components/concept/ZaplaHomepageContinuationV6.tsx
+++ src/components/concept/ZaplaHomepageContinuationV6.tsx
@@ -1,7 +1,7 @@
 import { ZaplaCustomerJourneyShowcaseV6 } from "@/components/concept/ZaplaCustomerJourneyShowcaseV6";
 import { ZaplaCustomerJourneyPrototypeSmoothV6 } from "@/components/concept/ZaplaCustomerJourneyPrototypeSmoothV6";
 import { ZaplaRevenueLeakageV7 } from "@/components/concept/ZaplaRevenueLeakageV7";
-import { ZaplaAIConversationsV6 } from "@/components/concept/ZaplaAIConversationsV6";
+import ZaplaAIConversationsV6 from "@/components/concept/ZaplaAIConversationsV6";
 import { ZaplaGrowthV6, ZaplaUnlimitedV6 } from "@/components/concept/ZaplaGrowthUnlimitedV6";
 import { ZaplaFinalV6, ZaplaGuidedLaunchV6, ZaplaProofV6 } from "@/components/concept/ZaplaClosingV6";
 
