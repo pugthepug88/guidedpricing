8e869d33675e22eed83748b965e6a4c101f61bf3
pugthepug88 <jinrcao@gmail.com>
2026-09-03 16:34:48 +1000

    Use focused repeat business section in V6

--- src/components/concept/ZaplaHomepageContinuationV6.tsx
+++ src/components/concept/ZaplaHomepageContinuationV6.tsx
@@ -3,7 +3,8 @@ import { ZaplaCustomerJourneyPrototypeSmoothV6 } from "@/components/concept/Zapl
 import { ZaplaRevenueLeakageV7 } from "@/components/concept/ZaplaRevenueLeakageV7";
 import ZaplaAIConversationsV6 from "@/components/concept/ZaplaAIConversationsV6";
 import ZaplaAIReceptionistV6 from "@/components/concept/ZaplaAIReceptionistV6";
-import { ZaplaGrowthV6, ZaplaUnlimitedV6 } from "@/components/concept/ZaplaGrowthUnlimitedV6";
+import ZaplaRepeatBusinessV6 from "@/components/concept/ZaplaRepeatBusinessV6";
+import { ZaplaUnlimitedV6 } from "@/components/concept/ZaplaGrowthUnlimitedV6";
 import { ZaplaFinalV6, ZaplaGuidedLaunchV6, ZaplaProofV6 } from "@/components/concept/ZaplaClosingV6";
 
 export function ZaplaHomepageContinuationV6() {
@@ -14,7 +15,7 @@ export function ZaplaHomepageContinuationV6() {
       <ZaplaCustomerJourneyPrototypeSmoothV6 />
       <ZaplaAIConversationsV6 />
       <ZaplaAIReceptionistV6 />
-      <ZaplaGrowthV6 />
+      <ZaplaRepeatBusinessV6 />
       <ZaplaUnlimitedV6 />
       <ZaplaGuidedLaunchV6 />
       <ZaplaProofV6 />
