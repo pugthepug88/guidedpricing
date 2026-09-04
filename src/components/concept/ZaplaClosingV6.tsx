7dc59e27e0cf5a84aa26a09dc08580909f3d9ad4
pugthepug88 <jinrcao@gmail.com>
2026-09-04 11:53:36 +1000

    Keep Guided Launch image contained

--- src/components/concept/ZaplaClosingV6.tsx
+++ src/components/concept/ZaplaClosingV6.tsx
@@ -61,7 +61,7 @@ export function ZaplaGuidedLaunchV6() {
 
         <div className="relative flex min-w-0 justify-end">
           <img
-            src="/concept/guided-launch-people-v6-hq.webp"
+            src="/concept/guided-launch-people-v6-final.webp"
             alt="Business owner working with a Zapla launch specialist"
             className="block h-auto w-full max-w-[760px] object-contain"
             loading="lazy"
